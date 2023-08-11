225daaff10fe12a9730e511ef2606791aa499c64
Rouhollah Mazarei <shahtrend01@gmail.com>
2023-08-11 17:12:22 +0430

    Get token from server

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,11 +6,21 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index()
     {
         return view('index');
+    }
+
+    public function getData()
+    {
+    	$response = Http::post('http://chart.pishrun.ir/api/login', [
+    		'email'		=> 'a@b.com',
+    		'password'	=> 'test@test'
+    	]);
+    	return $response;
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,43 @@
 
 @section('content')
 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+            	<input type="text" name="date-from" class="datepicker">
+            </div>
+            <div class="col-md-2">
+            	<input type="text" name="date-to" class="datepicker">
+            </div>
+            <div class="col-md-2">
+            	<button class="btn btn-default get-data">Get Data</button>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+@section('script')
+	<script type="text/javascript">
+		$(function(){
+			$(".datepicker").persianDatepicker()
+
+			$(".get-data").on('click', function(){
+				$.ajax({
+					type: "POST",
+					url: '/getData',
+					data: {
+						_token: "{{ csrf_token() }}"
+					},
+					success: function(data){
+						console.log(data)
+					},,
+                    error: function (xhr, status, error) {
+                        console.log(error);
+                    }
+				});
+			})
+
+		})
+	</script>
 @endsection
